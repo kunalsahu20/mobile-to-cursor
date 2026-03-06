00df53ffd19ae403d1b46c221441a4dfcaacf30b
kunal <kunalsahu1980@gmail.com>
2026-03-07 00:12:49 +0530

    Bump version to v1.0.9 (copyright year fix)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vexra.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
     }
 
     buildTypes {
